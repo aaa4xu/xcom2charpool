1ddd85e1b4c813fa483842ff490308e6fabd50cf
Aleksey Orekhovsky <aaa4xu@gmail.com>
2026-02-01 18:53:17 +0700

    Simplify CharacterPoolRegistry constructor by removing non-vanilla arrays

--- src/CharacterPool/CharacterPoolRegistry.ts
+++ src/CharacterPool/CharacterPoolRegistry.ts
@@ -9,12 +9,7 @@ import { Registry } from '../Core/Registry';
 export class CharacterPoolRegistry extends Registry {
     public constructor() {
         super();
-        this.registerArray('CharacterPool', TypedArray.of(StructProperty));
-        this.registerArray('ExtraDatas', TypedArray.of(StructProperty));
-        this.registerArray('AppearanceStore', TypedArray.of(StructProperty));
-        this.registerArray('CharacterPoolLoadout', TypedArray.of(StructProperty));
-        this.registerArray('UniformSettings', TypedArray.of(StructProperty));
-        this.registerArray('CosmeticOptions', TypedArray.of(StructProperty));
         this.registerArray('CharacterPoolDataElements', TypedArray.of(StructProperty));
+        this.registerArray('CharacterPool', TypedArray.of(StructProperty));
     }
 }
