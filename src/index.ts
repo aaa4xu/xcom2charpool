b0c05055f54a9e2d8cee7d4262b6b878717c0341
Aleksey Orekhovsky <aaa4xu@gmail.com>
2026-02-04 18:46:42 +0700

    Refactor library exports

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-export * from './CharacterPool/CharacterPoolPacker';
-export * from './CharacterPool/CharacterPoolUnpacker';
-export * from './CharacterPool/CharacterPoolRegistry';
-export * from './CharacterPool/CharacterPool';
+export * from './Files/CharacterPoolFile';
+export * from './Files/CharacterPoolFileWithIAM';
 
-export * from './Core/Registry';
+export * from './Schema/CharacterPoolSchema';
+export * from './Schema/CharacterPoolDataItemSchema';
+export * from './Schema/TAppearanceSchema';
+export * from './Schema/IAMCharacterPoolSchema';
+
+export * from './Registry';
+export * from './Codec';
+export * from './CodecContext';
+export * from './TypeCodec';
+export * from './ArrayElementCodec';
+export * from './BaseCodec';
 
 export * from './Reader';
 export * from './Writer';
-
-export * from './Core/Packer';
-export * from './Core/Unpacker';
-
 export * from './ArrayBuffer/ArrayBufferReader';
 export * from './ArrayBuffer/ArrayBufferWriter';
 
-export * from './Core/PropertyFactory';
-export * from './Core/Properties/ArrayProperty';
-export * from './Core/Properties/BoolProperty';
-export * from './Core/Properties/ByteProperty';
-export * from './Core/Properties/IntProperty';
-export * from './Core/Properties/NameProperty';
-export * from './Core/Properties/NoneProperty';
-export * from './Core/Properties/StrProperty';
-export * from './Core/Properties/StructProperty';
-export * from './Core/Arrays/ArrayFactory';
-export * from './Core/Arrays/TypedArray';
+export * from './Properties/Struct/StructArrayElement';
+export * from './Properties/Byte/ByteArrayElement';
+export * from './Properties/Int/IntArrayElement';
 
-export * from './Schema/CharacterPoolDataItemSchema';
-export * from './Schema/CharacterPoolSchema';
-export * from './Schema/NamePropertySchema';
-export * from './Schema/StructPropertySchema';
-export * from './Schema/TAppearanceSchema';
+export * from './Properties/Struct/StructPropertyValue';
+export * from './Properties/Name/NamePropertyValue';
+export * from './Properties/Byte/BytePropertyValue';
+export * from './Properties/Array/ArrayValue';
+
+export * from './Properties/Struct/StructSchema';
 export * from './Properties/Array/ArrayOfStructSchema';
+export * from './Properties/Array/ArraySchema';
+export * from './Properties/Name/NameSchema';
+export * from './Properties/Byte/ByteSchema';
+
+export * from './Errors/CodecError';
