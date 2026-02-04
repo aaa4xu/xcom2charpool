311220590adcd1dd1a50f27267ce9809961d7568
Aleksey Orekhovsky <aaa4xu@gmail.com>
2026-02-04 19:16:06 +0700

    Remove unused IntProperty import from BoolProperty.spec.ts

--- src/Properties/BoolProperty.spec.ts
+++ src/Properties/BoolProperty.spec.ts
@@ -1,7 +1,6 @@
 import { ArrayBufferReader } from '../ArrayBuffer/ArrayBufferReader';
 import { ArrayBufferWriter } from '../ArrayBuffer/ArrayBufferWriter';
 import { BoolProperty } from './BoolProperty';
-import { IntProperty } from './Int/IntProperty';
 import { CodecRegistry } from '../Registry';
 
 describe('BoolProperty', () => {
